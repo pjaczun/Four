39f544d44014245381318a9be94c1263a5053a65
Piotr <pjaczun@gmail.com>
2021-07-08 15:37:53 +0200

    some stuff

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Four app is running smoothly!!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Four app is running smoothly!!!');
   });
 });
